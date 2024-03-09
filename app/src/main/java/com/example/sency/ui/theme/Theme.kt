f66accf4a91771ccbf1e1032956901539988429f
Dorin Dorsman <dorin.dorsman@intel.com>
2024-03-09 21:16:25 +0200

    Project setyp

---
+++ app/src/main/java/com/example/sency/ui/theme/Theme.kt
@@ -0,0 +1,33 @@
+package com.example.sency.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = primaryColor,
+    primaryVariant = primaryDarkColor,
+    secondary = secondaryColor,
+    secondaryVariant = secondaryDarkColor,
+)
+
+private val LightColorPalette = lightColors(
+    primary = primaryColor,
+    primaryVariant = primaryLightColor,
+    secondary = secondaryColor,
+    secondaryVariant = secondaryLightColor,
+)
+
+@Composable
+fun SencyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) DarkColorPalette else LightColorPalette
+
+        MaterialTheme(
+            colors = colors,
+            typography = Typography,
+            shapes = Shapes,
+            content = content
+        )
+}
